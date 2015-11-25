6c986c370771a973e3fb0d652e0f114366e82a05
cocoissong <songkehui_@126.com>
2015-11-25 09:00:07 +0800

    try
    try git

---
+++ ExtendedCalendarView/src/com/tyczj/extendedcalendarview/Start.java
@@ -0,0 +1,18 @@
+package com.tyczj.extendedcalendarview;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Start extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.mycalendar);
+		
+	}
+	
+	
+
+}
